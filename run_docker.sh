8d17ecd43d7ba3131df91f09146d9029945d4341
Matthias Tytgat <matthias.tytgat@gmail.com>
2020-04-06 16:28:44 +0200

    run_docker.sh updated

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
 # Step 1:
 # Build image and add a descriptive tag
 
-# Step 2: 
+# Step 2:
+docker build --tag=bohousepred .
+# Step 2:
 # List docker images
 
-# Step 3: 
+# Step 3:
+docker image ls
+# Step 3:
 # Run flask app
+docker run -p 8000:5001 bohousepred
