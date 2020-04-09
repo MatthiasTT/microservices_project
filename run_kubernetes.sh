29e59299c7906f417b158c8869eae1cbcfef127d
Matthias Tytgat <matthias.tytgat@gmail.com>
2020-04-09 10:13:24 +0200

    run_kubernetes.sh populated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="matthiastt/bohousepred:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run bohousepred\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=bohousepred
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward bohousepred 80:80
